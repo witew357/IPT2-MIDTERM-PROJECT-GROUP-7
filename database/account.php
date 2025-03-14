b681a0458205a032eff3dca95686481a326555f0
RIGM-Domain <rigm.domain@gmail.com>
2025-03-15 02:28:15 +0800

    adding file for registration

---
+++ database/account.php
@@ -0,0 +1,14 @@
+<?php
+  $server_name = "localhost";
+  $db_name = "iphone16";
+  $db_username = "root";
+  $db_password = "";
+
+  $conn = new mysqli($server_name, $db_username, $db_password, $db_name);
+  
+  if($conn->connect_error){
+    die("Database connection failed. " . $conn->connect_error);
+  }
+
+
+?>
