3a01851bbdc02d32cb81035c27f84a7b06a87bd3
RIGM-Domain <rigm.domain@gmail.com>
2025-03-16 09:36:26 +0800

    adding js modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -14,3 +14,20 @@ viewIphoneModal.addEventListener('show.bs.modal', function (event) {
     document.getElementById('view-Storage').textContent = Storage;
     document.getElementById('view-Price').textContent = Price;
 });
+
+var editIphoneModal = document.getElementById('editIphoneModal');
+editIphoneModal.addEventListener('show.bs.modal', function (event) {
+    var button = event.relatedTarget;
+    var id = button.getAttribute('data-id');
+    var Variants = button.getAttribute('data-Variants');
+    var Colors = button.getAttribute('data-Colors');
+    var Storage = button.getAttribute('data-Storage');
+    var Price = button.getAttribute('data-Price');
+
+    document.getElementById('edit-id-display').textContent = id; // Use textContent for safety
+    document.getElementById('edit-id').value = id;
+    document.getElementById('edit-Variants').value = Variants;
+    document.getElementById('edit-Colors').value = Colors;
+    document.getElementById('edit-Storage').value = Storage;
+    document.getElementById('edit-Price').value = Price;
+});
