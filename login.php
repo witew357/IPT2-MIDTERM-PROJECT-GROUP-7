e015d9100687f84bd4195006864885421aed7457
witew35 <gariljasmin@gmail.con>
2025-03-20 16:06:20 +0800

    fixing the hover for log in

--- login.php
+++ login.php
@@ -182,7 +182,7 @@ function togglePassword() {
                 <!-- You can delete the links only if you purchased the pro version. -->
                 <!-- Licensing information: https://bootstrapmade.com/license/ -->
                 <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                Designed by <a href="https://AppleZone.ph/">AppleZone.PH</a>
               </div>
 
             </div>
